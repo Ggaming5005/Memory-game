a0233843146f2d1f72b721b6cc27e0f34bd9071a
Ggaming5005 <ggaming5005@gmail.com>
2025-04-04 13:29:49 -0700

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GameOptionsProvider from "./components/GameOptionsProvider";
+import OptionsSelectedProvider from "./components/OptionsSelectedProvider";
+import App from "./components/App/App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./abstracts.css";
+import "./composition.css";
+import "./global.css";
+import "./reset.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GameOptionsProvider>
+      <OptionsSelectedProvider>
+        <App />
+      </OptionsSelectedProvider>
+    </GameOptionsProvider>
+  </React.StrictMode>
+);
